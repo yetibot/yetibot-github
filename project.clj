1b09c75225ee73740586b52520ac54e57f794911
Trevor Hartman <trevorhartman@gmail.com>
2023-04-10 14:19:58 -0600

    Add ability to fetch a single repo
    Fix https://github.com/yetibot/yetibot-github/issues/2

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/yetibot/yetibot-github"
   :plugins [[lein-parent "0.3.8"]
             [lein-inferv "20201020.015531.74d2ced"]]
-  :parent-project {:coords [yetibot/parent "20201026.012801.efa80f2"]
+  :parent-project {:coords [yetibot/parent "20201027.011403.84290c7"]
                    :inherit [[:profiles :dev]
                              :license
                              :repl-options
@@ -12,4 +12,4 @@
                              :dependencies
                              :managed-dependencies]}
   :dependencies [[district0x/graphql-query "1.0.6"]]
-  :profiles {:provided {:dependencies [[irresponsible/tentacles "0.6.6"]]}})
+  :profiles {:provided {:dependencies [[irresponsible/tentacles "0.6.9"]]}})
