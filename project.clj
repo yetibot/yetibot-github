53d932088591e8d872150430537205dfe8c64b8e
Trevor Hartman <trevorhartman@gmail.com>
2023-04-18 18:58:32 -0600

    Add scm

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject yetibot/github "_"
   :description "A yetibot plugin for https://github.com"
   :url "https://github.com/yetibot/yetibot-github"
+  :scm {:name "git" :url "https://github.com/yetibot/yetibot-github"}
   :plugins [[lein-parent "0.3.9"]
             [lein-inferv "20201020.015531.74d2ced"]]
   :parent-project {:coords [yetibot/parent "20230419.003943.a089c31"]
